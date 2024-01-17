3866fa024a17c490462178ccc2a2060298639b35
Trang25112003 <lttrang25@gmail.com>
2024-01-17 10:26:25 +0700

    nop bai tap

---
+++ on tap/b2/index.js
@@ -0,0 +1,3 @@
+document.getElementById('demo1').innerHTML = 'Tai lieu hoc CSS'
+document.getElementById('demo2').innerHTML = 'Tai lieu hoc MySQL'
+document.getElementById('demo3').innerHTML = 'Tai lieu hoc PHP'
---
+++ on tap/b3/vidu.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = 25
+var c = a + b
+
+document.write('c= ' + c)
+var x=  100 + 200
+document.write('x= ' + x)
+
+var a = prompt("nhap chieu dai: ","0" )
+var b = prompt("nhap chieu rong: ","0" )
+
+alert("Dien tich la: " + a*b)
